5eaf9baf0af99cb437711445b58301e8aa7562f9
dafma <mar_tp@hotmail.es>
2018-01-15 08:23:31 -0600

    hasta create user

--- clients/views.py
+++ clients/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 
 from django.shortcuts import render, redirect
-from .forms import LoginForm
+from .forms import LoginForm, CreateUSerForm
 from django.contrib.auth import authenticate, login as login_django
 from django.contrib.auth import logout as logout_django
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -13,6 +13,8 @@ def show(request):
 
 
 def login(request):
+    if request.user.is_authentificated():
+        return redirect('client:dashboard')
     message = None
 
     if request.method == 'POST':
@@ -31,15 +33,23 @@ def login(request):
     }
     return render(request, 'login.html', context)
 
+@login_required( login_url= 'client:login' )
 def dashboard(request):
     context = {
 
     }
     return render(request, 'dashboard.html', context)
 
-
+@login_required( login_url= 'client:login' )
 def logout(request):
     logout_django(request)
     return redirect('client:login')
+
+def create(request):
+    form = CreateUSerForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'create.html', context)
 
 
