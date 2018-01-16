e5cc207a7864d91683f7c563ffeae198a34ec061
dafma <mar_tp@hotmail.es>
2018-01-16 07:24:31 -0600

    refactorizar funcion login a vista login

--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 
 from django.shortcuts import render, redirect
+from django.views import View
+
 from .forms import LoginForm, CreateUSerForm
 from django.contrib.auth import authenticate, login as login_django
 from django.contrib.auth import logout as logout_django
@@ -11,6 +13,29 @@ from django.contrib.auth.decorators import login_required
 def show(request):
     return HttpResponse("Hola desde el cliente")
 
+class LoginView(View):
+    form = LoginForm()
+    message = None
+    template = 'login.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('client:dashboard')
+        return render(request, self.template, self.get_context())
+
+    def post(self, request, *args, **kwargs):
+        username_post = request.POST['username']
+        password_post = request.POST['password']
+        user = authenticate(username=username_post, password=password_post)
+        if user is not None:
+            login_django(request, user)
+            return redirect('client:dashboard')
+        else:
+            self.message = "Usernam o password incorrecto"
+        return render(request, self.template, self.get_context())
+
+    def get_context(self):
+        return {'form': self.form, 'message':self.message}
 
 def login(request):
 
