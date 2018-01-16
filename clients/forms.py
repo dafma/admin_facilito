bf57241d30ad510170ca2ac6ac4d8a963b11e30f
dafma <mar_tp@hotmail.es>
2018-01-16 06:49:34 -0600

    add required

--- clients/forms.py
+++ clients/forms.py
@@ -3,10 +3,17 @@ from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=20)
-    password = forms.CharField(max_length=20)
+    password = forms.CharField(max_length=20, widget= forms.PasswordInput())
 
 
 class CreateUSerForm(forms.ModelForm):
+    username = forms.CharField(max_length=20,
+                               error_messages= {'required': 'El username es requerido',
+                                                'unique': 'El username ya se encuentra registrado',
+                                                'invalid':'el usrname es incorrecto'})
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput(),
+                               error_messages={'required': 'El password es requerido'})
+    email = forms.CharField(error_messages={'required': 'El email es requerido', 'invalid': 'ingrese un correo valido'})
     class Meta:
         model = User
         fields = ('username', 'password', 'email')
